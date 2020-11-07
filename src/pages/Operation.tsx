8c7a014aaddaf742c39f31b5d6959c17daa4203c
rurujiadedashuaiguo <13938338016@163.com>
2020-11-07 10:25:25 +0800

    操作页面

---
+++ src/pages/Operation.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './index.less';
+import 'antd/dist/antd.css'
+
+
+const Operation = React.createClass({
+  propTypes: {
+
+  },
+
+  getDefaultProps() {
+    return {
+      
+    };
+  },
+
+  render() {
+    return (
+      <div>Opeartion</div>
+    )
+  }
+})
+
+export default Operation;
